650ace5ec8239e736f399d55ef1bbab6b17ad2cf
leonardopadilha <leonardopadilha28@gmail.com>
2021-11-27 16:56:27 -0300

    Adding new test scenarios

--- test/testFindPet.spec.js
+++ test/testFindPet.spec.js
@@ -1,6 +1,7 @@
 import chai from "chai";
 import chaiHttp from "chai-http";
 import * as constant from "../suport/constants";
+import * as requestCreatePet from "../request/requestCreatePet";
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -8,13 +9,32 @@ const expect = chai.expect;
 const request = chai.request(constant.URL_API)
 
 describe("Pesquisar por um pet", () => {
-    context("quando pesquiso por um pet existente", () => {
-        it("então o pet é exibido com sucesso", (done) => {
+    context("quando altero um pet com sucesso", () => {
+        before((done) => {
             request
-                .get(constant.PET + "/" + 1)
+                .put(constant.PET)
+                .send(requestCreatePet.createPet(6, "Cachorro", "New Dog III",
+                    "http://www.teste.com",
+                    "animais domésticos",
+                    "available"
+                ))
                 .end((err, res) => {
                     expect(res).to.has.status(200)
+                    expect(res.body.id).to.equals(6)
                     expect(res.body.category.name).to.equals("Cachorro")
+                    expect(res.body.name).to.equals("New Dog III")
+                    done();
+                })
+        })
+
+        it("e realizo a pesquisa desse PET, então o pet é exibido com sucesso", (done) => {
+            request
+                .get(constant.PET + "/" + 6)
+                .end((err, res) => {
+                    expect(res).to.has.status(200)
+                    expect(res.body.id).to.equals(6)
+                    expect(res.body.category.name).to.equals("Cachorro")
+                    expect(res.body.name).to.equals("New Dog III")
                     done();
                 })
         })
@@ -25,12 +45,10 @@ describe("Pesquisar por um pet", () => {
             request
                 .get(constant.PET + "/" + 0)
                 .end((err, res) => {
-                    expect((err, res) => {
-                        expect(res).to.has.status(404)
-                        expect(res.body.type).to.equals("error")
-                        expect(res.body.message).to.equals("Pet not found")
-                        done();
-                    })
+                    expect(res).to.has.status(404)
+                    expect(res.body.type).to.equals("error")
+                    expect(res.body.message).to.equals("Pet not found")
+                    done();
                 })
         })
     })
