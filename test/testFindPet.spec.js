cfe21b072c278cf74a7daeb8e275eee03309f03a
leonardopadilha <leonardopadilha28@gmail.com>
2021-11-27 16:22:56 -0300

    Adding new validation

--- test/testFindPet.spec.js
+++ test/testFindPet.spec.js
@@ -19,4 +19,19 @@ describe("Pesquisar por um pet", () => {
                 })
         })
     })
+
+    context("quando pesquiso por um pet que não existe", () => {
+        it("então o sistema deverá retornar mensagem de erro", (done) => {
+            request
+                .get(constant.PET + "/" + 0)
+                .end((err, res) => {
+                    expect((err, res) => {
+                        expect(res).to.has.status(404)
+                        expect(res.body.type).to.equals("error")
+                        expect(res.body.message).to.equals("Pet not found")
+                        done();
+                    })
+                })
+        })
+    })
 })
